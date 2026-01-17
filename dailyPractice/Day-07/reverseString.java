a59505b10befcf3980f9d3bd018f91e737ea2802
Atharv <atharvgupta568@gmail.com>
2026-01-18 00:58:04 +0530

    Reverse printing of string

---
+++ dailyPractice/Day-07/reverseString.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class reverseString {
+    public static void reverseString(String name, int i){
+        if(i<0){
+            return;
+        }
+        System.out.print(name.charAt(i));
+        reverseString(name, i-1);
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name=sc.next();
+        reverseString(name, name.length()-1);
+    }
+}
